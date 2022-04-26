5b76902ef33172cf3de830642dfa5c1e4feb6ab1
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-04-26 12:09:58 -0500

    Add Nullable and NonNull

---
+++ src/main/java/banana/internal/annotation/Nullable.java
@@ -0,0 +1,11 @@
+package banana.internal.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PARAMETER})
+public @interface Nullable {
+}
