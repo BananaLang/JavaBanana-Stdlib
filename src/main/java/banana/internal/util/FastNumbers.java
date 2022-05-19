3316e36e659975429833840f9ed96eaee78a3c97
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-05-19 11:09:32 -0500

    Create FastNumbers

---
+++ src/main/java/banana/internal/util/FastNumbers.java
@@ -0,0 +1,42 @@
+package banana.internal.util;
+
+import banana.builtin.Int;
+
+public final class FastNumbers {
+    private FastNumbers() {
+    }
+
+    public static final Int $_16 = Int.valueOf(-16);
+    public static final Int $_15 = Int.valueOf(-15);
+    public static final Int $_14 = Int.valueOf(-14);
+    public static final Int $_13 = Int.valueOf(-13);
+    public static final Int $_12 = Int.valueOf(-12);
+    public static final Int $_11 = Int.valueOf(-11);
+    public static final Int $_10 = Int.valueOf(-10);
+    public static final Int $_9 = Int.valueOf(-9);
+    public static final Int $_8 = Int.valueOf(-8);
+    public static final Int $_7 = Int.valueOf(-7);
+    public static final Int $_6 = Int.valueOf(-6);
+    public static final Int $_5 = Int.valueOf(-5);
+    public static final Int $_4 = Int.valueOf(-4);
+    public static final Int $_3 = Int.valueOf(-3);
+    public static final Int $_2 = Int.valueOf(-2);
+    public static final Int $_1 = Int.valueOf(-1);
+    public static final Int $0 = Int.valueOf(0);
+    public static final Int $1 = Int.valueOf(1);
+    public static final Int $2 = Int.valueOf(2);
+    public static final Int $3 = Int.valueOf(3);
+    public static final Int $4 = Int.valueOf(4);
+    public static final Int $5 = Int.valueOf(5);
+    public static final Int $6 = Int.valueOf(6);
+    public static final Int $7 = Int.valueOf(7);
+    public static final Int $8 = Int.valueOf(8);
+    public static final Int $9 = Int.valueOf(9);
+    public static final Int $10 = Int.valueOf(10);
+    public static final Int $11 = Int.valueOf(11);
+    public static final Int $12 = Int.valueOf(12);
+    public static final Int $13 = Int.valueOf(13);
+    public static final Int $14 = Int.valueOf(14);
+    public static final Int $15 = Int.valueOf(15);
+    public static final Int $16 = Int.valueOf(16);
+}
